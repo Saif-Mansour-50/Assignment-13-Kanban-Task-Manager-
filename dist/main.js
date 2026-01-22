08c769d115e54757e206954f445c779d39eed833
Saif-Mansour-50 <saif200716@gmail.com>
2026-01-22 18:56:27 +0200

    task-manager-ts

---
+++ dist/main.js
@@ -0,0 +1,12 @@
+let username = "saifo";
+let zzz = 15;
+let ss = "sa";
+let swsw = true;
+let sad = "sad";
+let sadas = true;
+function sayhello(user) {
+    console.log("hiiii", user);
+}
+sayhello("saif");
+let namesss = "aya";
+//# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+let username: string = "saifo";
+let zzz: number = 15;
+let ss: string = "sa";
+let swsw: boolean = true;
+let sad: string = "sad";
+let sadas: boolean = true;
+function sayhello(user: string) {
+  console.log("hiiii", user);
+}
+sayhello("saif");
+let namesss: string = "aya";
